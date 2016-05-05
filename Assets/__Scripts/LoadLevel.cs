36a81edfdcc2aa89f8af7a7ed7e629ff78865acc
eddiemac01 <eddiemac01@gmail.com>
2016-05-05 06:05:26 -0400

    Omega Mage Final
    All spells working, added intro screen

---
+++ Assets/__Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown)
+			Application.LoadLevel ("__OmegaMage_Scene_0");
+	
+	}
+}
